707d74dbec5049003bb8f2038b16a0d50c2a66ea
theboynextdoor <dyearwoodd@gmail.com>
2018-07-29 22:23:25 +0000

    Create PlusMinusButton component

--- src/PlusMinusButton.js
+++ src/PlusMinusButton.js
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PlusMinusButton = props => {
-    return (
-        <form className="plus-minus"> 
-            <button className="btn--decrement" onClick={props.onClickingDecrementButton}>-</button>
-            <input type="text" value={props.quantity} onChange={props.onChangingQuantity} />
-            <button className="btn--increment" onClick={props.onClickingIncrementButton}>+</button>
-        </form>
+    return(
+        <form className="plus-minus-button"> 
+            <button className="btn btn--plus" onClick={props.onClickPlusButton}>-</button>
+            <input type="text" value={props.numberField} required onChange={props.onChangeNumberField} />
+            <button className="btn btn--minus" onClick={props.onClickMinusButton}>+</button>
+        </form>    
     );
 }
 
 PlusMinusButton.propTypes = {
-    onClickingDecrementButton: PropTypes.func.isRequired, 
-    onClickingIncrementButton: PropTypes.func.isRequired, 
-    onChangingQuantity: PropTypes.func.isRequired, 
-    quantity: PropTypes.string.isRequired
-};
+    numberField: PropTypes.number.isRequired, 
+    onClickPlusButton: PropTypes.func.isRequired, 
+    onClickMinusButton: PropTypes.func.isRequired, 
+    onChangeNumberField: PropTypes.func.isRequired,
+}
 
 export default PlusMinusButton; 
