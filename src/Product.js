da5ac579971edb9fa4d356fd8ab81609127fd28c
theboynextdoor <dyearwoodd@gmail.com>
2018-08-03 20:27:17 +0000

    Create Product component

---
+++ src/Product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Product = (props) => {
+    return (
+        <li className="product">
+            <img className="product__img" src={props.src} alt={props.alt} />
+            <div className="product__info">
+                <h2 className="product__name">{props.name}</h2>
+                <p className="product__price">{props.price}</p>
+            </div>
+            <button className="product__btn" onClick={props.onClick}>Add to cart</button>
+        </li>
+    ); 
+};
+
+export default Product; 
